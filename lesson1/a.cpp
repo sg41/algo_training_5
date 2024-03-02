74d2d93e3772aa6f8165a57f5402ea653308ec04
Serge <joellebo@student.21-school.ru>
2024-03-02 18:54:57 +0700

    a

--- lesson1/a.cpp
+++ lesson1/a.cpp
@@ -3,14 +3,25 @@
 #include <iostream>
 int max_trees(int p, int v, int q, int m) {
   int vasya_start, vasya_end, masha_start, masha_end;
-  int kt = 0;
-  if (k < p) vasya_start = p - std::abs(v);
-  vasya_end = p + std::abs(v);
-  masha_start = q - std::abs(m);
-  masha_end = q + std::abs(m);
-  int global_start = std::min(vasya_start, masha_start);
-  int global_end = std::max(vasya_end, masha_end);
-  return std::abs(global_start) + std::abs(global_end) + 1;
+  m = std::abs(m);
+  v = std::abs(v);
+  if (q > p) {
+    std::swap(p, q);
+    std::swap(v, m);
+  } else if (q == p) {
+    return std::max(v, m) * 2 + 1;
+  }
+  vasya_start = p - v;
+  vasya_end = p + v;
+  masha_start = q - m;
+  masha_end = q + m;
+  if (vasya_start <= masha_end) {
+    int global_start = std::min(vasya_start, masha_start);
+    int global_end = std::max(vasya_end, masha_end);
+    return std::abs(global_start) + std::abs(global_end) + 1;
+  } else {
+    return 2 * v + 2 * m + 2;
+  }
 }
 int main(void) {
   int p, v, q, m;
