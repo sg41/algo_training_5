91c18224517053ee56796176751b45803fa46ae4
Serge <joellebo@student.21-school.ru>
2024-03-02 21:42:09 +0700

    b

--- lesson1/b.py
+++ lesson1/b.py
@@ -11,7 +11,8 @@ else:
     score1_home, score2_away = map(int,match2.split(':'))
 total1 = score1_home + score1_away
 total2 = score2_home + score2_away
-goals_difference = score2_home+score2_away - score1_home - score1_away
+goals_difference = total2 - total1
+away_difference = score2_away - score1_away
 if goals_difference == 0 :
     if score1_away > score2_away:
         print(0)
@@ -21,4 +22,16 @@ else:
     if total1 > total2:
         print(0)
     else:
-        
+        if score1_away > score2_away:
+            print(goals_difference)
+        else:
+            if first_game == kHome:
+                if goals_difference >= away_difference:
+                    print(goals_difference)
+                else:
+                    print(goals_difference+1)
+            else:
+                if goals_difference >= away_difference:
+                    print(goals_difference+1)
+                else:
+                    print(goals_difference)
