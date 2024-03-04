4585dd9ef80d57e65aa4251b943e5cb7ccb92efd
Joellebo <joellebo@student.21-school.ru>
2024-03-04 22:41:45 +0700

    h

--- lesson1/h.cpp
+++ lesson1/h.cpp
@@ -5,6 +5,7 @@ int main(void) {
   long double time = -1;
   if (v1 - v2 != 0) {
     time = (x2 - x1) / (v1 - v2);
+    std::cout.precision(10);
     std::cout << "YES\n" << time << "\n";
   } else {
     std::cout << "No";
