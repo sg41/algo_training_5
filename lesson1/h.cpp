64464f4a0f1b31851fce4be6aa09c266ab0aae12
Joellebo <joellebo@student.21-school.ru>
2024-03-04 22:40:09 +0700

    h

--- lesson1/h.cpp
+++ lesson1/h.cpp
@@ -2,6 +2,12 @@
 int main(void) {
   long double L, x1, v1, x2, v2;
   std::cin >> L >> x1 >> v1 >> x2 >> v2;
-
+  long double time = -1;
+  if (v1 - v2 != 0) {
+    time = (x2 - x1) / (v1 - v2);
+    std::cout << "YES\n" << time << "\n";
+  } else {
+    std::cout << "No";
+  }
   return 0;
 }
