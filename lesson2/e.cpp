af58042d751c624d98b47a8a0b644fe17e689b64
Joellebo <joellebo@student.21-school.ru>
2024-03-08 15:36:31 +0700

    e

--- lesson2/e.cpp
+++ lesson2/e.cpp
@@ -37,13 +37,26 @@ bool sort_by_high_positive(BerryIndex a, BerryIndex b) {
   if ((delta_a >= 0 && delta_b >= 0) || (delta_a < 0 && delta_b < 0)) {
     if (a.second.first > b.second.first) {
       result = true;
-      // } else if (a.second.first == b.second.first) {
-      //   if (delta_a > delta_b) {
-      //     result = true;
-      //   }
     }
   } else if ((delta_a >= 0 && delta_b < 0)) {
     result = true;
+  }
+  return result;
+}
+bool sort_by_low_positive(BerryIndex a, BerryIndex b) {
+  bool result = false;
+  long long delta_a = a.second.first - a.second.second;
+  long long delta_b = b.second.first - b.second.second;
+  if ((delta_a >= 0 && delta_b >= 0)) {
+    if (a.second.first < b.second.first) {
+      result = true;
+    }
+  } else if ((delta_a >= 0 && delta_b < 0)) {
+    result = true;
+  } else if (delta_a < 0 && delta_b < 0) {
+    if (a.second.first > b.second.first) {
+      result = true;
+    }
   }
   return result;
 }
@@ -56,7 +69,6 @@ bool sort_by_positive_delta_first(BerryIndex a, BerryIndex b) {
   } else if (delta_b < 0) {
     if (a.first > b.first) result = true;
   }
-
   return result;
 }
 long long count_max_height(std::vector<BerryIndex> berry_index) {
@@ -69,6 +81,37 @@ long long count_max_height(std::vector<BerryIndex> berry_index) {
                      berry_index[i].second.second;
   }
   return max_height;
+}
+
+long long dp(std::vector<BerryIndex>& berry_index) {
+  int n = berry_index.size();
+  std::vector<int> path(n + 1, 0);
+  struct {
+    long long h = 0;
+    long long max = 0;
+  } dp[n + 1][n];
+  long long max = 0;
+  long long max_h = 0;
+  int max_h_index = 0;
+  for (int i = 1; i < n; i++) {
+    long long cur_max = 0;
+    long long cur_max_h = 0;
+    int cur_max_h_index = 0;
+    for (int j = 0; j < n; j++) {
+      dp[i][j].max = max_h + berry_index[j].second.first;
+      dp[i][j].h =
+          max_h + berry_index[j].second.first - berry_index[j].second.second;
+      if (dp[i][j].max > cur_max_h) {
+        cur_max_h_index = j;
+        cur_max = dp[i][j].max;
+        cur_max_h = dp[i][j].h;
+        path[i] = j;
+      }
+    }
+    max = cur_max;
+    max_h = cur_max_h;
+    max_h_index = cur_max_h_index;
+  }
 }
 
 int main(void) {
@@ -86,7 +129,7 @@ int main(void) {
   long long max_height = count_max_height(berry_index);
 
   auto berry_index_copy = berry_index;
-  std::sort(berry_index.begin(), berry_index.end(), sort_by_high_positive);
+  std::sort(berry_index.begin(), berry_index.end(), sort_by_low_positive);
   long long max_heigt2 = count_max_height(berry_index);
   if (max_heigt2 > max_height) {
     max_height = max_heigt2;
