3668c81f643830ff7e5c457973a2750ed499e7fa
Serge <joellebo@student.21-school.ru>
2024-03-09 17:36:06 +0700

    e TL

--- lesson2/e.cpp
+++ lesson2/e.cpp
@@ -71,6 +71,17 @@ bool sort_by_positive_delta_first(BerryIndex a, BerryIndex b) {
   }
   return result;
 }
+bool sort_by_positive_second_first(BerryIndex a, BerryIndex b) {
+  bool result = false;
+  long long delta_a = a.second.first - a.second.second;
+  long long delta_b = b.second.first - b.second.second;
+  if (delta_a > 0) {
+    if (a.second.second > b.second.second) result = true;
+  } else if (delta_b < 0) {
+    if (a.second.second > b.second.second) result = true;
+  }
+  return result;
+}
 long long count_max_height(std::vector<BerryIndex> berry_index) {
   long long max_height = berry_index[0].second.first;
   long long current_height = max_height - berry_index[0].second.second;
@@ -79,6 +90,24 @@ long long count_max_height(std::vector<BerryIndex> berry_index) {
         std::max(max_height, current_height + berry_index[i].second.first);
     current_height = current_height + berry_index[i].second.first -
                      berry_index[i].second.second;
+  }
+  return max_height;
+}
+long long count_max_height(std::vector<BerryIndex> berry_index,
+                           std::vector<BerryIndex> berry_index_minus) {
+  long long max_height = berry_index[0].second.first;
+  long long current_height = max_height - berry_index[0].second.second;
+  for (int i = 1; i < berry_index.size(); i++) {
+    max_height =
+        std::max(max_height, current_height + berry_index[i].second.first);
+    current_height = current_height + berry_index[i].second.first -
+                     berry_index[i].second.second;
+  }
+  for (int i = 0; i < berry_index_minus.size(); i++) {
+    max_height = std::max(max_height,
+                          current_height + berry_index_minus[i].second.first);
+    current_height = current_height + berry_index_minus[i].second.first -
+                     berry_index_minus[i].second.second;
   }
   return max_height;
 }
@@ -121,28 +150,55 @@ int main(void) {
   std::cin >> n;
   std::vector<Berry> berrys(n);
   std::vector<BerryIndex> berry_index(n);
+  std::vector<BerryIndex> berry_index_plus;
+  std::vector<BerryIndex> berry_index_minus;
   for (int i = 0; i < n; i++) {
     std::cin >> berrys[i].first >> berrys[i].second;
     berry_index[i].first = i;
     berry_index[i].second = berrys[i];
+    if (berrys[i].first - berrys[i].second >= 0) {
+      berry_index_plus.push_back(berry_index[i]);
+    } else {
+      berry_index_minus.push_back(berry_index[i]);
+    }
   }
-  std::sort(berry_index.begin(), berry_index.end(), sort_by_delta);
-
+  std::sort(berry_index_plus.begin(), berry_index_plus.end(),
+            sort_by_low_positive);
+  std::sort(berry_index_minus.begin(), berry_index_minus.end(),
+            sort_by_low_positive);
   long long max_height = count_max_height(berry_index);
-
-  auto berry_index_copy = berry_index;
-  std::sort(berry_index.begin(), berry_index.end(), sort_by_low_positive);
-  long long max_heigt2 = count_max_height(berry_index);
-  if (max_heigt2 > max_height) {
-    max_height = max_heigt2;
-  } else {
-    berry_index = berry_index_copy;
+  for (int i = 0; i < berry_index_plus.size(); i++) {
+    std::swap(berry_index_plus[i],
+              berry_index_plus[berry_index_plus.size() - 1]);
+    long long new_max = count_max_height(berry_index_plus, berry_index_minus);
+    if (new_max > max_height) {
+      max_height = new_max;
+    } else {
+      std::swap(berry_index_plus[berry_index_plus.size() - 1],
+                berry_index_plus[i]);
+    }
   }
+
+  // auto berry_index_copy = berry_index;
+  // std::sort(berry_index.begin(), berry_index.end(), sort_by_low_positive);
+  // long long max_heigt2 = count_max_height(berry_index);
+  // if (max_heigt2 > max_height) {
+  //   max_height = max_heigt2;
+  // } else {
+  //   berry_index = berry_index_copy;
+  // }
+
   // max_height = dp(berry_index);
 
   std::cout << max_height << std::endl;
-  for (int i = 0; i < n; i++) {
-    std::cout << berry_index[i].first + 1 << " ";
+  // for (int i = 0; i < n; i++) {
+  //   std::cout << berry_index[i].first + 1 << " ";
+  // }
+  for (auto berry : berry_index_plus) {
+    std::cout << berry.first + 1 << " ";
+  }
+  for (auto berry : berry_index_minus) {
+    std::cout << berry.first + 1 << " ";
   }
   std::cout << std::endl;
   return 0;
