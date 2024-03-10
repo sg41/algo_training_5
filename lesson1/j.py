0261d7c04e9d5777307564fea1611299de22dc00
Joellebo <joellebo@student.21-school.ru>
2024-03-10 20:37:03 +0700

    1j in progress

--- lesson1/j.py
+++ lesson1/j.py
@@ -3,16 +3,18 @@ from sys import stdin
 
 
 def place_images(w, h, c, document):
-    current_h = h
     image_list = []
-    fragment = {"fragment_width": 0, "fragment_height": h,
-                "fragment_start_x": 0,
-                "fragment_start_y": 0}
+    fragment = {"fragment_width": 0, "height": h,
+                "start_x": 0,
+                "start_y": 0}
 
     def get_possible_width(x, y):
         result = w-x
         min = w-x
-        for image in image_list:
+        for i, image in enumerate(image_list):
+            if image["y"]+image["height"] < y:
+                image_list.pop(i)
+                continue
             if x < image["x"] and y > image["y"] and y < image["y"]+image["height"]:
                 result = image["x"]-x
                 if result < min:
@@ -21,24 +23,30 @@ def place_images(w, h, c, document):
 
     def create_fragment(start_x, start_y):
         nonlocal fragment
+        for i, image in enumerate(image_list):
+            if image["y"]+image["height"] < start_y:
+                image_list.pop(i)
+                continue
+            if start_x > image["x"] and start_x < image["x"]+image["width"] and image["y"] > start_y and image["y"] < start_y+fragment["height"]:
+                x = image["x"]+image["width"]
+                break
         x = start_x
         if x >= w:
             x = 0
+
         if x == 0:
-            y = start_y+fragment["fragment_height"]
+            y = start_y+fragment["height"]
         else:
             y = start_y
 
-        fragment = {"fragment_width": get_possible_width(x, y), "fragment_height": h,
-                    "fragment_start_x": x,
-                    "fragment_start_y": y}
+        fragment = {"width": get_possible_width(x, y), "height": h,
+                    "start_x": x,
+                    "start_y": y}
         # new current_y, current_x
-        return current_y+fragment["fragment_height"], 0
+        return current_y+fragment["height"], 0
 
     def process_paragraph(paragraph, current_y, current_x):
         create_fragment(current_x, current_y)
-        # fragment={"fragment_width": w, "fragment_height": current_h,
-        #                  "fragment_start_x": current_x, "fragment_start_y": current_y}
         for element in paragraph:
             if element.startswith("(image"):
                 image_params = parse_image_parameters(element)
@@ -47,18 +55,17 @@ def place_images(w, h, c, document):
             else:
                 words = element.split()
                 for word in words:
-                    current_fragment = fragment
-                    fragment_width = current_fragment["fragment_width"]
-                    fragment_start_x = current_fragment["fragment_start_x"]
+                    fragment_width = fragment["width"]
                     word_width = len(word) * c
                     if word_width+current_x > fragment_width:
                         # Word doesn't fit in the current fragment, move to the next line
                         # TODO look for the next fragment o create new one
-                        current_y, current_x = create_fragment(0, current_y)
+                        current_y, current_x = create_fragment(
+                            word_width+current_x+fragment["start_x"], current_y)
                         pass
 
                     # place the word
-                    if current_x != fragment_start_x:
+                    if current_x != 0:
                         # Add space before the word (except for the first word in the fragment)
                         current_x += c
                     current_x += word_width
@@ -83,37 +90,36 @@ def place_images(w, h, c, document):
     def place_image(image_params, current_y, current_x):
         layout = image_params["layout"]
         width, height = int(image_params["width"]), int(image_params["height"])
-        fragment_start_x = fragment["fragment_start_x"]
-        fragment_width = fragment["fragment_width"]
-        x, y = fragment_start_x+current_x, current_y
+        # fragment_start_x = fragment["start_x"]
+        # fragment_width = fragment["fragment_width"]
+        x, y = fragment["start_x"]+current_x, current_y
         if layout == "embedded":
-            if width+current_x > fragment_width:
+            if width+current_x > fragment["width"]:
                 # Word doesn't fit in the current fragment, move to the next line
                 # TODO look for the next fragment o create new one
-                current_y, current_x = create_fragment(0, current_y)
-                fragment_start_x = fragment["fragment_start_x"]
-                fragment_width = fragment["fragment_width"]
+                current_y, current_x = create_fragment(
+                    width+current_x+fragment["start_x"], current_y)
+                # fragment_start_x = fragment["start_x"]
+                # fragment_width = fragment["fragment_width"]
                 pass
 
-            if current_x != fragment_start_x:
+            if current_x != fragment["start_x"]:
                 # Add space before the image (except for the first word in the fragment)
                 current_x += c
             # Размещение рисунка с типом "embedded"
             if height > h:
                 # Если высота рисунка больше текущей высоты строки, увеличиваем высоту строки
-                fragment["fragment_height"] = height
+                fragment["height"] = height
             print(f"{current_x} {current_y}")
             return current_y, current_x + width  # Update current_x
 
         elif layout == "surrounded":
             # Размещение рисунка с типом "surrounded"
-            if width+current_x > fragment_width:
+            if width+current_x > fragment["width"]:
                 # Word doesn't fit in the current fragment, move to the next line
                 # TODO look for the next fragment o create new one
                 current_y, current_x = create_fragment(0, current_y)
-                fragment_start_x = fragment["fragment_start_x"]
-                fragment_width = fragment["fragment_width"]
-                x, y = fragment_start_x+current_x, current_y
+                x, y = fragment["start_x"]+current_x, current_y
                 pass
             image_list.append(
                 {"x": x, "y": y, "width": width, "height": height})
@@ -133,6 +139,7 @@ def place_images(w, h, c, document):
                 # Если рисунок выходит за правую границу страницы, смещаем его влево
                 x = width-w
             y += dy
+            x += dx
             print(f"{x} {y}")
             return current_y, current_x  # Update current_x
 
