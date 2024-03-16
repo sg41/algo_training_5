add17eafa5d1e5567bb4a9397509b57e751169a2
Joellebo <joellebo@student.21-school.ru>
2024-03-16 20:28:16 +0700

    a.cpp

--- lesson3/a.cpp
+++ lesson3/a.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
+#include <set>
+#include <string>
+#include <vector>
 int main(void) {
   int n;
+  std::cin >> n;
+  std::vector<std::set<std::string>> playlist(n);
+  for (int i = 0; i < n; i++) {
+    int k;
+    std::cin >> k;
+    for (int j = 0; j < k; j++) {
+      std::string song;
+      std::cin >> song;
+      playlist[i].insert(song);
+    }
+  }
+  std::set<std::string> result{playlist[0]};
+  for (int i = 1; i < n; i++) {
+    std::set<std::string> current_result;
+    std::set_intersection(
+        playlist[i].begin(), playlist[i].end(), result.begin(), result.end(),
+        std::inserter(current_result, current_result.begin()));
+    result = current_result;
+    if (result.size() == 0) {
+      break;
+    }
+  }
+  if (result.size() == 0) {
+    std::cout << "NO" << std::endl;
+  } else {
+    std::cout << "YES" << std::endl;
+    for (auto song : result) {
+      std::cout << song << " ";
+    }
+  }
   return 0;
 }
