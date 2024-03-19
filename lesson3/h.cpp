b398aeff1874216584ba8dde826851abbb448c50
Joellebo <joellebo@student.21-school.ru>
2024-03-19 22:42:46 +0700

    h test 12 TL

--- lesson3/h.cpp
+++ lesson3/h.cpp
@@ -44,7 +44,6 @@ int move_picture(
   int dy = b_coord[i].first.second - a_coord[i].first.second;
   std::vector<std::pair<Coord, Coord>> a_coord_new(a_coord.size());
   for (int j = 0; j < a_coord.size(); j++) {
-    // if (j == i) continue;
     a_coord_new[j] = a_coord[j];
     a_coord_new[j].first.first += dx;
     a_coord_new[j].first.second += dy;
@@ -64,28 +63,42 @@ int main(void) {
   std::unordered_multiset<std::pair<Coord, Coord>> b_set_coord(n);
   std::vector<std::pair<int, int>> a_vector(n);
   std::vector<std::pair<int, int>> b_vector(n);
-  std::unordered_multiset<std::pair<int, int>> b_set_vector(n);
+  std::unordered_multiset<std::pair<int, int>> a_set_vector(n);
   int start_x_a = 0, start_y_a = 0;
   for (int i = 0; i < n; i++) {
     std::cin >> a[i].x1 >> a[i].y1 >> a[i].x2 >> a[i].y2;
+    if(a[i].x1 > a[i].x2){
+        std::swap(a[i].x1, a[i].x2);
+        std::swap(a[i].y1, a[i].y2);
+    } else if (a[i].x1 == a[i].x2 && a[i].y1 > a[i].y2) {
+        std::swap(a[i].x1, a[i].x2);
+        std::swap(a[i].y1, a[i].y2);
+    }
     a_vector[i] = {std::make_pair(a[i].x2 - a[i].x1, a[i].y2 - a[i].y1)};
     a_coord[i] = {
         std::make_pair(std::make_pair(a[i].x1, a[i].y1), a_vector[i])};
+    a_set_vector.insert(a_vector[i]);
   }
   int start_x_b, start_y_b;
   for (int i = 0; i < n; i++) {
     std::cin >> b[i].x1 >> b[i].y1 >> b[i].x2 >> b[i].y2;
+    if(b[i].x1 > b[i].x2){
+        std::swap(b[i].x1, b[i].x2);
+        std::swap(b[i].y1, b[i].y2);
+    } else if (b[i].x1 == b[i].x2 && b[i].y1 > b[i].y2) {
+        std::swap(b[i].x1, b[i].x2);
+        std::swap(b[i].y1, b[i].y2);
+    }
     b_vector[i] = {std::make_pair(b[i].x2 - b[i].x1, b[i].y2 - b[i].y1)};
     b_coord[i] = {
         std::make_pair(std::make_pair(b[i].x1, b[i].y1), b_vector[i])};
     b_set_coord.insert(b_coord[i]);
-    b_set_vector.insert(b_vector[i]);
   }
 
   int done_count = 0;
   for (int i = 0; i < n; i++) {
-    if (std::find(b_set_vector.begin(), b_set_vector.end(), a_vector[i]) !=
-        b_set_vector.end()) {
+    if (std::find(a_set_vector.begin(), a_set_vector.end(), b_vector[i]) !=
+        a_set_vector.end()) {
       done_count =
           std::max(done_count, move_picture(a_coord, b_coord, i, b_set_coord));
     }
