3f940455ede64d5d6b0c6a691cf45f101d0e9795
Joellebo <joellebo@student.21-school.ru>
2024-03-27 13:55:58 +0700

    h_tests.py

---
+++ lesson4/h_tests.py
@@ -0,0 +1,49 @@
+import subprocess
+import argparse
+import timeit
+
+task_name = "h.cpp"
+
+
+def run_test(data, **kwargs):
+    if kwargs.get("comment") != None:
+        print(kwargs.get("comment"))
+    test = subprocess.Popen(
+        "./a.out",  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    (out, err) = test.communicate(input=data)
+    if test.returncode != 0:
+        print(data)
+        print(err)
+        print("RE")
+    # print(data)
+    print(out)
+    res = list(out.split("\n"))
+    if kwargs.get("answer") and len(res) > 0:
+        if res != kwargs.get("answer").split("\n"):
+            answer = kwargs.get("answer").split("\n")
+            print(f"WRONG ANSWER: {res} != {answer}")
+            return
+
+    if kwargs.get("value") != None and len(res) > 1:
+        if "\n".join(res[1:-1]) != kwargs.get("value"):
+            print(
+                f"WRONG VALUE: {res[1:]} != {kwargs.get('value')}")
+
+
+parser = argparse.ArgumentParser(description='My example explanation')
+parser.add_argument('-d', '--debug', action='store_true', help='Debug mode')
+args = parser.parse_args()
+if args.debug:
+    result = subprocess.run(["g++", "-std=c++20", "-g", task_name])
+else:
+    result = subprocess.run(["g++", "-std=c++20", "-g", "-O3",  task_name])
+if result.returncode == 0:
+
+    run_test("""3
+7 -1
+2 8
+1 2
+""", answer="""6
+3
+3 2 5
+""", comment="test 1")
